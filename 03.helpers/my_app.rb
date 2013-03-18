884df9417ea32920adb2868da3e30383af3752ba
sonots <sonots@gmail.com>
2013-03-18 20:01:39 +0900

    03.helpers

---
+++ 03.helpers/my_app.rb
@@ -0,0 +1,16 @@
+# my_app.rb
+require 'sinatra/base'
+require_relative 'lib/sinatra/htmlescape'
+
+class MyApp < Sinatra::Base
+  helpers Sinatra::HTMLEscapeHelper
+
+  get '/hello' do
+    h 'Hello World!'
+  end
+  
+  get '/hello/:name' do
+    h "Hello #{params[:name]}!"
+  end
+end
+
